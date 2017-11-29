c7e4b9f689efa4039c220f450782c13d8f1d366f
Gabriel Fernandes <gabrielfernndss@gmail.com>
2017-11-28 23:37:19 -0300

    modal

--- js/controllers/list_playlists.js
+++ js/controllers/list_playlists.js
@@ -1,7 +1,11 @@
 app.controller('list-playlists', function($scope, playlists) {
     $scope.playlists = playlists.playlists
+    $scope.remPlaylistNome = ''
 
-    $scope.remPlaylist = function(playlist) {
-        playlists.remPlaylist(playlist)
+    $scope.setRemPlaylistNome = function(playlist) {
+        $scope.remPlaylistNome = playlist
+    }
+    $scope.remPlaylist = function() {
+        playlists.remPlaylist($scope.remPlaylistNome)
     }
 })

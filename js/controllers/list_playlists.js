d754efdbcad0c27526d167bd3382c798ee967b89
Gabriel Fernandes <gabrielfernndss@gmail.com>
2017-11-28 22:45:58 -0300

    badges

--- js/controllers/list_playlists.js
+++ js/controllers/list_playlists.js
@@ -1,2 +1,7 @@
 app.controller('list-playlists', function($scope, playlists) {
+    $scope.playlists = playlists.playlists
+
+    $scope.remPlaylist = function(playlist) {
+        playlists.remPlaylist(playlist)
+    }
 })

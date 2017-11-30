a821233cab069069df786d9104220fa7bb51ff9c
Gabriel Fernandes <gabrielfernndss@gmail.com>
2017-11-29 22:28:18 -0300

    ultima ouvida

--- js/controllers/artista.js
+++ js/controllers/artista.js
@@ -1,8 +1,8 @@
 app.controller('artista', function($scope, $routeParams, artistas) {
     $scope.artista = artistas.artistas[artistas.indexOf($routeParams.nome)]
-    $scope.musicas = $scope.artista.getMusicas()
     $scope.nota = $scope.artista.nota
-    $scope.musica = $scope.artista.ultimaOuvida
+    $scope.album = $scope.artista.ultimaOuvida.album
+    $scope.musica = $scope.artista.ultimaOuvida.musica
     $scope.favorito = $scope.artista.favorito
 
     $scope.favoritar = function() {
@@ -23,10 +23,16 @@ app.controller('artista', function($scope, $routeParams, artistas) {
         else
             $scope.artista.ultimaOuvida = -1
     })
-    $scope.$watch('musica', function() {
+    $scope.$watch('album', function() {
         if ($scope.musica)
-            $scope.artista.ultimaOuvida = $scope.musica
+            $scope.artista.ultimaOuvida.album = $scope.album
         else
             $scope.artista.ultimaOuvida = {}
+    })
+    $scope.$watch('musica', function() {
+        if ($scope.musica)
+            $scope.artista.ultimaOuvida.musica = $scope.musica
+        else
+            $scope.artista.ultimaOuvida.musica = {}
     })
 })

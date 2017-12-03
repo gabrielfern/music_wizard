f7da42fbd0d2079bf69d2875291011e895673fe2
Gabriel Fernandes <gabrielfernndss@gmail.com>
2017-12-03 03:23:47 -0300

    regex aware

--- js/controllers/list_artistas.js
+++ js/controllers/list_artistas.js
@@ -1,3 +1,21 @@
 app.controller('list-artistas', function($scope, artistas) {
     $scope.artistas = artistas.artistas
+    $scope.artistasToShow = [...artistas.artistas]
+
+    $scope.$watch('artistaPesquisa', function() {
+        if ($scope.artistaPesquisa) {
+            try {
+                let padrao = RegExp($scope.artistaPesquisa, 'i')
+                $scope.artistasToShow = $scope.artistas.filter(arts => {
+                    if (padrao.exec(arts.nome))
+                        return true
+                    return false
+                })
+            } catch(_) {
+
+            }
+        } else {
+            $scope.artistasToShow = [...artistas.artistas]
+        }
+    })
 })

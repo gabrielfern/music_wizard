74325038e78b88bcf3eca9d21091a8b93a299c9d
Gabriel Fernandes <gabrielfernndss@gmail.com>
2017-12-28 20:30:07 -0300

    example

--- src/main/java/io/wizard/music/Wizard.java
+++ src/main/java/io/wizard/music/Wizard.java
@@ -1,28 +1,12 @@
 package io.wizard.music;
 
-import org.springframework.boot.*;
-import org.springframework.boot.autoconfigure.*;
-import org.springframework.stereotype.*;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@Controller
-@EnableAutoConfiguration
+
+@SpringBootApplication
 public class Wizard {
-
-    @ResponseBody
-    @RequestMapping("/api/get")
-    String get(@RequestParam(value="name", defaultValue="") String name) {
-        return "working on it " + name;
-    }
-    
-    @ResponseBody
-    @RequestMapping("/api/post")
-    String post(@RequestBody String name) {
-    	System.out.println(name);
-        return "ok " + name;
-    }
-
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         SpringApplication.run(Wizard.class, args);
     }
 }

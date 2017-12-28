cdea3394dd3d312349c000a6f0d0f512e6cc26eb
Gabriel Fernandes <gabrielfernndss@gmail.com>
2017-12-28 16:15:00 -0300

    spring boot post handling

--- src/main/java/Wizard.java
+++ src/main/java/Wizard.java
@@ -7,6 +7,19 @@ import org.springframework.web.bind.annotation.*;
 @EnableAutoConfiguration
 public class Wizard {
 
+    @ResponseBody
+    @RequestMapping("/api/get")
+    String get(@RequestParam(value="name", defaultValue="") String name) {
+        return "working on it " + name;
+    }
+    
+    @ResponseBody
+    @RequestMapping("/api/post")
+    String post(@RequestBody String name) {
+    	System.out.println(name);
+        return "ok " + name;
+    }
+
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Wizard.class, args);
     }
